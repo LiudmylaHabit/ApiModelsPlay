899ef39a0824fa9090a8aa395e8bdc584e8abe30
HabitL <liudmylaha@wizardsdev.com>
2021-10-12 18:09:26 +0300

    wgr node connection

--- NbitcOinWagerrPlay2/Program.cs
+++ NbitcOinWagerrPlay2/Program.cs
@@ -13,8 +13,10 @@ namespace NbitcOinWagerrPlay2
 			Console.WriteLine("Hello World!");
 			//HashingAlgo.MainHash();
 			//SendGetBlockRequest(1852102);
-			SendGetTrxRequest("960d852781bdabef826617530e0d09444e6d2b806a5c224a9c7667f2cae202ad");
-			SendGetTrxRequestTryies("960d852781bdabef826617530e0d09444e6d2b806a5c224a9c7667f2cae202ad");
+			//SendGetTrxRequest("960d852781bdabef826617530e0d09444e6d2b806a5c224a9c7667f2cae202ad");
+			//SendGetTrxRequestTryies("960d852781bdabef826617530e0d09444e6d2b806a5c224a9c7667f2cae202ad");
+			RPCHelper.GetBlockThroughRPC();
+			Console.ReadKey();
 		}
 
 		static void SendGetTrxRequestTryies(string txNumber)
@@ -39,7 +41,7 @@ namespace NbitcOinWagerrPlay2
 			Transaction wagerrTrnx;
 			try
 			{
-				wagerrTrnx = NBitcoin.Transaction.Parse("0100000001ca4b52683c9b77e81c375a2fad5bc9cf382bc06fde4f48c9b62dddba4d956b28000000006b48304502210096d942a91f9a30ecf50f1bbfd80b561a31a0da0eddaa7a735bdd345f2fba23aa02200b745e2438fd85232f64400f80f7d13df04af0eaef229853775dbd235a806bd30121024f2c027ed6a61bc78b0c311f7284c7e46febf191c2510a70ec00bd428586d788ffffffff02c0b9ee2b020000001976a9149f0242d5b780cbcba16794a27f9d90908531347b88ac0000000000000000156a13420105b26801007cc400000c3000000000000000000000",
+				wagerrTrnx = Transaction.Parse("0100000001ca4b52683c9b77e81c375a2fad5bc9cf382bc06fde4f48c9b62dddba4d956b28000000006b48304502210096d942a91f9a30ecf50f1bbfd80b561a31a0da0eddaa7a735bdd345f2fba23aa02200b745e2438fd85232f64400f80f7d13df04af0eaef229853775dbd235a806bd30121024f2c027ed6a61bc78b0c311f7284c7e46febf191c2510a70ec00bd428586d788ffffffff02c0b9ee2b020000001976a9149f0242d5b780cbcba16794a27f9d90908531347b88ac0000000000000000156a13420105b26801007cc400000c3000000000000000000000",
 					Network.Main);
 			}
 			catch (Exception) { }
---
+++ NbitcOinWagerrPlay2/RPCHelper.cs
@@ -0,0 +1,41 @@
+﻿using NBitcoin;
+using NBitcoin.RPC;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace NbitcOinWagerrPlay2
+{
+    public static class RPCHelper
+    {
+
+        public static string ConnectToRPC()
+        {
+            var creds = new NetworkCredential("", "");
+            var rpcClient = new RPCClient(Network.Main);
+            var rpcCreds = RPCClient.GetRPCAuth(creds);
+            return rpcCreds;
+        }
+
+        public static void GetBlockThroughRPC()
+        {
+			RPCClient rpc;
+			try
+			{
+				//http://52.224.84.119:55003/
+				//rpc0 = new NBitcoin.RPC.RPCClient("", "127.0.0.1:18332");
+				//rpc = new NBitcoin.RPC.RPCClient("test123:test123", uriOne, Network.Main);
+				rpc = new RPCClient(ConnectToRPC(), "http://52.224.84.119:55003/", Network.Main);
+				var blpck = rpc.GetBlock(1852102);
+			}
+			catch (Exception) { }
+            //try
+            //{
+            //    var transaction = rpc.GetRawTransactionInfo("960d852781bdabef826617530e0d09444e6d2b806a5c224a9c7667f2cae202ad");
+            //}
+			
+		}
+    }
+}
